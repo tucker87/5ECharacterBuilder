19a2d2ab10c80de84f4f2fea8a14772e96c66717
unknown <Jason Tucker@COMP022014B.RA.local>
2014-08-28 11:31:34 -0400

    Initial Commit

---
+++ 5ECharacterBuilder/5ECharacterBuilderTests/CreateCharacterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5ECharacterBuilderTests
+{
+    [TestClass]
+    public class CreateCharacterTests
+    {
+        [TestMethod]
+        public void CharactersCanHaveNames()
+        {
+            var character = new Character();
+            Assert.AreEqual("", character.Name);
+            character.Name = "John";
+            Assert.AreEqual("John", character.Name);
+            character = new Character(hitDice:"d4", name:"John");
+            Assert.AreEqual("John", character.Name);
+        }
+
+        [TestMethod]
+        public void CharactersMustHaveHitDice()
+        {
+            var character = new Character(hitDice: "d8");
+        }
+    }
+
+    public class Character
+    {
+        public Character()
+        {
+            Name = "";
+        }
+        public Character(string hitDice, string name = "")
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
